eaed1b2e2f98581c9def698d3ccf25eadb98be59
Juan Tobar <jtobar1964@gmail.com>
2013-04-17 20:35:25 -0400

    Added more comments

--- createFindIndexes_20130416.py
+++ createFindIndexes_20130416.py
@@ -2,12 +2,12 @@
 # =========================================================================================
 # Script : createFindIndexes.py
 # -----------------------------------------------------------------------------------------
-# Purpose : This script creates a an xml of unique feature class feature names to be used
-#           with RegGSS Finds.
+# Purpose : This script creates an xml of unique feature class feature names to be
+#           loaded into the RegGSS - Find Canals (AHED) listbox.
 # -----------------------------------------------------------------------------------------
-# Suppliment Info:
+# Supplemental Info:
 # -----------------------------------------------------------------------------------------
-# Supervisor : Juan Tobar, Regulation SFWMD, (561)682-6687
+# Supervisor : Juan Tobar, Regulation GIS, SFWMD
 # Create date: 9 April 2013
 #------------------------------------------------------------------------------------------
 
@@ -15,12 +15,10 @@
 import sys
 import string
 import os
-#import shutil
-#import pyodbc
 import arcpy
-#import time
 
 def createFindIndexes():
+    #XML Elements
     from xml.etree import ElementTree
     from xml.etree.ElementTree import Element
     from xml.etree.ElementTree import SubElement
@@ -35,22 +33,29 @@ def createFindIndexes():
     rows = arcpy.SearchCursor("Database Connections/gisuser@ghydp_ghydpsde.sde/AHED.HYDROEDGE", "", "", "NAME")
     for row in rows:
         strValue = str(row.getValue("NAME"))
+        # remove junk feature names
         if strValue != 'None' and strValue != "<Null>" and strValue != ' ' and strValue != '':
             valueList.append(strValue)    
 
     del rows
     del row
     
+    # set is used to create a unique list of values
     uniqueSet = set(valueList)
+    # convert it back to a list so it can be sorted
     uniqueList = list(uniqueSet)
     uniqueList.sort()
 
+    # iterate through the list constructing the SubElements
     for s in uniqueList:
         # <configuration><feature_class><feature_name/>
         SubElement( feature_class, 'AHED.HYDROEDGE', feature_name='%s' %s)
 
+    # create the XML output
     output_file = open( '//ad.sfwmd.gov/dfsroot/data/err_gis/scratch/jtobar/ahed_hydroedge_index.xml', 'w' )
+    # you have to add the xml version manually
     output_file.write( '<?xml version="1.0"?>' )
+    # but the rest is automated
     output_file.write( ElementTree.tostring( configuration ) )
     output_file.close()
     
